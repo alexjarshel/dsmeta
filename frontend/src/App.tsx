996ce9eca863b744b1c34fd97893fb8ba1acc048
unknown <alexbaukartjarschel@gmail.com>
2022-11-18 12:08:16 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton/inedx"
+
 function App() {
   return(
-    <h1>ola mundo</h1>
+    <>
+      <h1>ola mundo</h1>
+      <NotificationButton/>
+    </>
   )
 }
 
