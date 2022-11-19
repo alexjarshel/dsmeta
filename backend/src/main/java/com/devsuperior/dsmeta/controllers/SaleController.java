0753e09507802d5ac45f32617447cbb322b6c4e8
unknown <alexbaukartjarschel@gmail.com>
2022-11-19 17:20:46 -0300

    herku

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SmsService;
 import com.devsuperior.dsmeta.services.saleService;
 
 @RestController
@@ -19,7 +21,8 @@ public class SaleController {
 	
 	@Autowired
 	private saleService service;
-	
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales(
@@ -27,6 +30,11 @@ public class SaleController {
 			@RequestParam(value="maxDate", defaultValue = "") String maxDate,
 			Pageable pageable){
 		return service.findSales(minDate, maxDate, pageable);
+	}
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
+		
 	}
 
 }
