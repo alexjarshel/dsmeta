e951c29e249c051fbf5b4cc8960343ac0c308afd
unknown <alexbaukartjarschel@gmail.com>
2022-11-20 15:52:58 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header/index";
 import SalesCard from "./components/SalesCard/index";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return(
     <>
+
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
